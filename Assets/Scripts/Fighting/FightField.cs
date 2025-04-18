6138c23220db7acfd7bbb6a49760f959977e7e18
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-04-18 12:24:13 +0300

    Add fight slot selection mechanic

--- Assets/Scripts/Fighting/FightField.cs
+++ Assets/Scripts/Fighting/FightField.cs
@@ -8,9 +8,22 @@ public class FightField : MonoBehaviour
     public static FightField Instance;
     [SerializeField] private List<FightSlot> fightSlots = new List<FightSlot>();
 
+    private FightSlot selectedSlot;
     void Awake()
     {
         Instance = this;
+
+        InitializeSlots();
+    }
+    private void InitializeSlots()
+    {
+        for (int i = 0; i < fightSlots.Count; i++)
+        {
+            fightSlots[i].SetIndex(i);
+            fightSlots[i].OnSelected += SelectSlot;
+        }
+
+        SelectSlot(0);
     }
     public IEnumerator PerformEnemiesTurn()
     {
@@ -18,5 +31,11 @@ public class FightField : MonoBehaviour
             yield return StartCoroutine(slot.RunEnemyTurn());
 
         yield return null;
+    }
+    private void SelectSlot(int index)
+    {
+        Debug.Log("Selected slot " + index);
+
+        selectedSlot = fightSlots[index];
     }
 }
--- Assets/Scripts/Fighting/FightSlot.cs
+++ Assets/Scripts/Fighting/FightSlot.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FightSlot : MonoBehaviour
 {
+    public event Action<int> OnSelected;
     [SerializeField] private Enemy enemy;
-
+    private int index;
     public IEnumerator RunEnemyTurn()
     {
         if (enemy != null && enemy.IsAlive())
             yield return StartCoroutine(enemy.MakeMove());
 
         yield return null;
+    }
+    public void SetIndex (int i) => index = i;
+    void OnMouseDown()
+    {
+        OnSelected?.Invoke(index);
     }
 }
